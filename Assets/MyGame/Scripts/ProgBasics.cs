29a65425f41f136379d5c5080ef64ee3597b01c8
kathrin <kathrin.koenig@htl-salzburg.ac.at>
2019-04-06 13:36:50 +0200

    Multiplikative Operatoren

---
+++ Assets/MyGame/Scripts/ProgBasics.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgBasics : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("erste Tests mit Operatoren");
+
+        Debug.Log("Multiplikative Operatoren: *"+(2 * 8));
+        Debug.Log("Multiplikative Operatoren: /"+(12/2));
+        Debug.Log("Multiplikative Operatoren: %"+(7%2));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
