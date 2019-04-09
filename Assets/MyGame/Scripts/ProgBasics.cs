858a989971c701e30ff8b135050337bb0a814390
kathrin <kathrin.koenig@htl-salzburg.ac.at>
2019-04-09 09:53:55 +0200

    update script

--- Assets/MyGame/Scripts/ProgBasics.cs
+++ Assets/MyGame/Scripts/ProgBasics.cs
@@ -10,27 +10,27 @@ public class ProgBasics : MonoBehaviour {
         Debug.Log("erste Tests mit Operatoren");
 
         // Multiplikative Operatoren
-        Debug.Log("Multiplikative Operatoren: *"+(2 * 8));
-        Debug.Log("Multiplikative Operatoren: /"+(12/2));
-        Debug.Log("Multiplikative Operatoren: %"+(7%2));
+        Debug.Log("Multiplikative Operatoren: * "+(2 * 8));
+        Debug.Log("Multiplikative Operatoren: / "+(12/2));
+        Debug.Log("Multiplikative Operatoren: % "+(7%2));
 
         // Additive Operatoren
-        Debug.Log("Additive Operatoren: +"+(3+6));
-        Debug.Log("Additive Operatoren: -" + (12-11));
+        Debug.Log("Additive Operatoren: + "+(3+6));
+        Debug.Log("Additive Operatoren: - " + (12-11));
 
         // Relationale Operatoren
-        Debug.Log("Relationale Operatoren: <" + (3 < 6));
-        Debug.Log("Relationale Operatoren: >" + (3 > 6));
-        Debug.Log("Relationale Operatoren: <=" + (3 <= 3));
-        Debug.Log("Relationale Operatoren: >=" + (3 >= 6));
+        Debug.Log("Relationale Operatoren: < " + (3 < 6));
+        Debug.Log("Relationale Operatoren: > " + (3 > 6));
+        Debug.Log("Relationale Operatoren: <= " + (3 <= 3));
+        Debug.Log("Relationale Operatoren: >= " + (3 >= 6));
 
         // Gleichheits Operatoren
-        Debug.Log("Gleichheits Operatoren: =" + ( "Blau" == "blau"));
-        Debug.Log("Gleichheits Operatoren: !=" + ("Blau" == "blau"));
+        Debug.Log("Gleichheits Operatoren: = " + ( "Blau" == "blau"));
+        Debug.Log("Gleichheits Operatoren: != " + ("Blau" == "blau"));
 
         // Logische Operatoren
-        Debug.Log("Logische Operatoren: &" + (true && false));
-        Debug.Log("Logische Operatoren: |" + (true || false));
+        Debug.Log("Logische Operatoren: & " + (true && false));
+        Debug.Log("Logische Operatoren: | " + (true || false));
     }
 	
 	// Update is called once per frame
