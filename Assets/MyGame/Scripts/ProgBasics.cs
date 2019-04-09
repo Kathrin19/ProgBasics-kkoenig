92f44554dde5d2f2293d3bf9b5e366c141fa795b
kathrin <kathrin.koenig@htl-salzburg.ac.at>
2019-04-09 09:15:40 +0200

    update script

--- Assets/MyGame/Scripts/ProgBasics.cs
+++ Assets/MyGame/Scripts/ProgBasics.cs
@@ -7,30 +7,30 @@ public class ProgBasics : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
-        
-
         Debug.Log("erste Tests mit Operatoren");
 
+        // Multiplikative Operatoren
         Debug.Log("Multiplikative Operatoren: *"+(2 * 8));
         Debug.Log("Multiplikative Operatoren: /"+(12/2));
         Debug.Log("Multiplikative Operatoren: %"+(7%2));
 
+        // Additive Operatoren
         Debug.Log("Additive Operatoren: +"+(3+6));
         Debug.Log("Additive Operatoren: -" + (12-11));
 
+        // Relationale Operatoren
         Debug.Log("Relationale Operatoren: <" + (3 < 6));
         Debug.Log("Relationale Operatoren: >" + (3 > 6));
         Debug.Log("Relationale Operatoren: <=" + (3 <= 3));
         Debug.Log("Relationale Operatoren: >=" + (3 >= 6));
 
+        // Gleichheits Operatoren
         Debug.Log("Gleichheits Operatoren: =" + ( "Blau" == "blau"));
         Debug.Log("Gleichheits Operatoren: !=" + ("Blau" == "blau"));
 
+        //// Logische Operatoren
         Debug.Log("Logische Operatoren: &" + (true && false));
-        Debug.Log("Multiplikative Operatoren: |" + (true || false));
-
-
-
+        Debug.Log("Logische Operatoren: |" + (true || false));
     }
 	
 	// Update is called once per frame
